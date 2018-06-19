c06dea7a8cc4d0dd81a375f0a447d0588ba3ac1a
claudiastenberg <32322760+claudiastenberg@users.noreply.github.com>
2018-06-19 09:52:09 +0200

    Första

---
+++ tomcla/src/main/java/se/claudiastenberg/tomcla/TomclaApplication.java
@@ -0,0 +1,12 @@
+package se.claudiastenberg.tomcla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomclaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomclaApplication.class, args);
+	}
+}
---
+++ tomcla/src/test/java/se/claudiastenberg/tomcla/TomclaApplicationTests.java
@@ -0,0 +1,16 @@
+package se.claudiastenberg.tomcla;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TomclaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
